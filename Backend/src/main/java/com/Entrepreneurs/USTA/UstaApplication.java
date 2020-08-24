9a6b3dfad5b3deddda5ed4ed98b6d5da96707e6a
nazik-elhag <elhag.nazik.com>
2020-08-23 22:08:12 -0400

    Spring Boot Project Setup

---
+++ Backend/src/main/java/com/Entrepreneurs/USTA/UstaApplication.java
@@ -0,0 +1,13 @@
+package com.Entrepreneurs.USTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Entrepreneurs/USTA/UstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Entrepreneurs.USTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
