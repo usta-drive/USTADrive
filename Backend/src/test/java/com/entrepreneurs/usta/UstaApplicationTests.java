7226689931ec9d0fc11c399cf3228a5d90409a9e
nazik-elhag <elhag.nazik.com>
2020-09-02 21:32:18 -0400

    added backend Java 8 Spring Boot initializer setup

---
+++ Backend/src/test/java/com/entrepreneurs/usta/UstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrepreneurs.usta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
