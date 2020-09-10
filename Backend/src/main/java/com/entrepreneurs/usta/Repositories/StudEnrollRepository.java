09547dda60fe93390d5c45fdaaddc3eb28e9d518
Tagwa Abdelrahman <tagwaesam5@gmail.com>
2020-09-10 14:15:50 -0400

    added init values for instructer,session,calender

--- Backend/src/main/java/com/entrepreneurs/usta/Repositories/StudEnrollRepository.java
+++ Backend/src/main/java/com/entrepreneurs/usta/Repositories/StudEnrollRepository.java
@@ -1,5 +1,9 @@
 package com.entrepreneurs.usta.Repositories;
 
-public interface StudEnrollRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.entrepreneurs.usta.Model.StudEnroll;
+
+public interface StudEnrollRepository extends CrudRepository<StudEnroll, Long> {
 
 }
