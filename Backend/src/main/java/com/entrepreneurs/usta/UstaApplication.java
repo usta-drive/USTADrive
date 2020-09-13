71fc17f8a909ffd0ed3a9e97aef8d209fbb984a6
nazik-elhag <elhag.nazik.com>
2020-09-12 20:39:57 -0400

    file format

--- Backend/src/main/java/com/entrepreneurs/usta/UstaApplication.java
+++ Backend/src/main/java/com/entrepreneurs/usta/UstaApplication.java
@@ -8,15 +8,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class UstaApplication extends SpringBootServletInitializer {
 
-	 @Override
-	 	     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	 
-	         return application.sources( UstaApplication.class);
-	 
-	     }
-	public static void main(String[] args)  {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UstaApplication.class);
+
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(UstaApplication.class, args);
-		
+
 	}
 
 }
